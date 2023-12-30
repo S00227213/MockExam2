2a20f8712ebfb90bb26bcd6f98a6c2bafb4e1732
Jack Monaghan <s00227213@atu.ie>
2023-12-30 16:19:48 +0000

    Add initial project files

---
+++ RAD301-Mock-Exam-2023-MVC-App-S00227213/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RAD301_Mock_Exam_2023_MVC_App_S00227213.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Rad301-Mock-Exam-2023-DataModel-S00227213/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Rad301_Mock_Exam_2023_DataModel_S00227213
+{
+    public class Class1
+    {
+
+    }
+}
