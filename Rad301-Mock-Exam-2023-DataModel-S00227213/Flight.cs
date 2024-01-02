abd9ff9d969c7cdaec5f8e0c69d287ba7dacbd35
Jack Monaghan <s00227213@atu.ie>
2024-01-02 20:56:00 +0000

    S00227213 Q1b

---
+++ Rad301-Mock-Exam-2023-DataModel-S00227213/Flight.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rad301_Mock_Exam_2023_DataModel_S00227213
+{
+    public class Flight
+    {
+        public int FlightId { get; set; }
+        public string FlightNumber { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public string Country { get; set; }
+        public int MaxSeats { get; set; }
+
+        // Navigation property for related Passengers
+        public List<Passenger> Passengers { get; set; }
+    }
+
+}
---
+++ Rad301-Mock-Exam-2023-DataModel-S00227213/Passenger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rad301_Mock_Exam_2023_DataModel_S00227213
+{
+    public class Passenger
+    {
+        public int PassengerId { get; set; }
+        public string Name { get; set; }
+        public string TicketType { get; set; }
+        public decimal TicketPrice { get; set; }
+        public decimal BaggageCharge { get; set; }
+        public int FlightId { get; set; }
+
+        // Navigation property for related Flight
+        public Flight Flight { get; set; }
+    }
+
+}
