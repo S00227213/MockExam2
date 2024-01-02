d07ecd3de8b93aa2e79b23d44a5b46bf32915403
Jack Monaghan <s00227213@atu.ie>
2024-01-02 21:47:58 +0000

    S00227213 Q1D

---
+++ RAD301-Mock-Exam-2023-MVC-App-S00227213/Controllers/CheckedInController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Rad301_Mock_Exam_2023_DataModel_S00227213; 
+using Tracker.WebAPIClient;
+
+public class CheckedInController : Controller
+{
+    private readonly FlightContext _context;
+
+    public CheckedInController(FlightContext context)
+    {
+        _context = context;
+
+        // Tracker call
+        ActivityAPIClient.Track(StudentID: "S00227213", StudentName: "Jack Monaghan",
+            activityName: "Rad301 Mock Exam 2023", Task: "Creating Checkin Controller");
+    }
+
+    // Action to show passenger details for a specific flight
+    public IActionResult Index(int flightId)
+    {
+        var passengers = _context.Passengers
+            .Include(p => p.Flight) // Include the Flight navigation property
+            .Where(p => p.FlightId == flightId)
+            .ToList();
+        return View(passengers);
+    }
+
+}
---
+++ Rad301-Mock-Exam-2023-DataModel-S00227213/FlightContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Rad301_Mock_Exam_2023_DataModel_S00227213
+{
+    public class FlightContextFactory : IDesignTimeDbContextFactory<FlightContext>
+    {
+        public FlightContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()) 
+                .AddJsonFile("appsettings.json") 
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<FlightContext>();
+            var connectionString = configuration.GetConnectionString("FlightContextConnection");
+
+            builder.UseSqlServer(connectionString);
+
+            return new FlightContext(builder.Options);
+        }
+    }
+}
